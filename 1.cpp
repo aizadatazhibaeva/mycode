72ff0f094df76d93d95c9ba28367261d43f25efc
aizadatazhibaeva <115732176+aizadatazhibaeva@users.noreply.github.com>
2022-10-21 23:33:45 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-using namepsace std;
-int main(){
-	cout<<"hello";
-   return 0;
-
+using namespace std;
+int main() {
+    int n,k=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }    for(int i=0; i<n; i++){
+            if(arr[i]>0){
+                k++;
+                
+            }
+        } cout<<k<<endl;
+    return 0;
 }
